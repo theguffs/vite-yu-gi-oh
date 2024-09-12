8b0a2ef531c442bc86c5ff5487b78fc0353bbd8b
theguffs <youssefabdelmalak01@gmail.com>
2024-09-12 17:04:23 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    IncrementCount(){
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+
+  <AppHeader/>
+
+  <button @click="IncrementCount()">
+
+  </button>
+  
+</div>
+</template>
+
+<style scoped>
+
+</style>
+npm,
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+// my-component.js
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!"
+    }
+  },
+}
+</script>
+
+<template>
+<header>
+  <h1>
+    {{ message }}
+  </h1>
+</header>
+</template>
+
+<style scoped>
+header{
+  background-color: greenyellow ;
+  text-align: center;
+}
+h1{
+  color: red;
+}
+</style>
+npm,
